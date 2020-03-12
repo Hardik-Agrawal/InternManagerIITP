b6f8bde6d90a9733c9f33a3bc203d1e3d61fd3b7
Hardik-Agrawal <43006581+Hardik-Agrawal@users.noreply.github.com>
2020-03-12 23:24:00 +0530

    Fixed project search issue (#14)
    * syncronized with new changes and algo
    
    * added database
    
    * Fixed the project search issue

--- appliedStudents.php
+++ appliedStudents.php
@@ -6,21 +6,60 @@ if (!logged_in()) {
     redirect("login.php");
 }
 $id = $_SESSION['id'];
-$projects = getParticularProfProjects($id);
+updatePhase($id);
+$phase = getProfPhase($id);
+var_dump($phase);
+$projects = array();
+
+if ($phase != 0) {
+    $projects = getParticularProfProjects($id, $phase);
+}
+if (isset($_POST['selectStudent'])) {
+    $student_id = $_POST['id'];
+    $proj_id = $_POST['proj_id'];
+    $query = "UPDATE students SET selected = 1,proj_id = '$proj_id',prof_id='$id' WHERE id = '$student_id'";
+    $res = query($query);
+    confirm($res);
+    for ($i = 1; $i <= 4; $i++) {
+        $query = "DELETE FROM prefrence_$i WHERE student_id = '$student_id'";
+        $res = query($query);
+        confirm($res);
+    }
+    $query = "SELECT email FROM students WHERE id = '$student_id'";
+    $res = query($query);
+    confirm($res);
+    $row = fetch_array($res);
+    $email = $row['email'];
+    $subject = "You have been selected";
+    $msg = "Intern mil gayi";
+    $headers = "";
+    send_email($email, $subject, $msg, $headers);
+}
 ?>
 <div class="container">
-    <h1>Projects</h1>
-    <?php foreach ($projects as $project) {
-        $id = $project['id'];
-        $students = getRegisteredStudents($id);
-    ?>
-        <?php if (count($students) !== 0) : ?>
-            <h3><?php echo $project['title'] ?></h3>
-            <ul class="list-group">
-                <?php foreach ($students as $student) : ?>
-                    <button type="button" class="list-group-item list-group-item-action"><?php echo $student[1]; ?></button>
-                <?php endforeach; ?>
-            <?php endif ?>
-            </ul>
-        <?php } ?>
+    <?php if ($phase === 0) : ?>
+        <h1>Selection Phase Over</h1>
+    <?php else : ?>
+        <h1>Projects</h1>
+        <?php foreach ($projects as $project) {
+            $title = getProjectTitle($project['project_id']);
+            $proj_id = $project['project_id'];
+            $students = getStudentsInPhase($id, $phase, $proj_id);
+        ?>
+            <?php if (count($students) !== 0) : ?>
+                <h3><?php echo $title ?></h3>
+                <ul class="list-group">
+                    <?php foreach ($students as $student) : ?>
+                        <form action="" method="POST">
+                            <p type="button" class="list-group-item list-group-item-action"><?php echo $student['first_name'] . "  " . $student['last_name']; ?>
+                                <input type="text" value=<?php echo $student['id'] ?> style="display:none;" name="id">
+                                <input type="text" value=<?php echo $proj_id ?> style="display:none;" name="proj_id">
+                                <input type="submit" value="Select" name="selectStudent" class="btn btn-primary">
+                            </p>
+                        </form>
+                    <?php endforeach; ?>
+                <?php endif ?>
+                </ul>
+            <?php } ?>
+        <?php endif; ?>
 </div>
--- functions/phpmail.php
+++ functions/phpmail.php
@@ -15,13 +15,13 @@ function send_php_mail($email, $subject, $msg, $headers)
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'vatsal.eliot@gmail.com';                     // SMTP username
-        $mail->Password   = 'mkaqaumuchffjcmh';                             // SMTP password
+        $mail->Username   = "projecthardik8@gmail.com";                     // SMTP username
+        $mail->Password   = "143830jsu";                             // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
         $mail->Port       = 587;                                    // TCP port to connect to
-  
+
         //Recipients
-        $mail->setFrom('ashyadavash@gmail.com', 'Intern-Manager IIT Patna');
+        $mail->setFrom('projecthardik8@gmail.com', 'Intern-Manager IIT Patna');
         $mail->addAddress($email);
 
         // Content
@@ -30,7 +30,7 @@ function send_php_mail($email, $subject, $msg, $headers)
         $mail->Body    = $msg;
         // $mail->AltBody = $msg;
 
-        if($mail->send()){
+        if ($mail->send()) {
             // echo 'Message has been sent';
             return true;
         } else {
